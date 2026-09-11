fec7dafdfa695150dd27986ac1de5082947d2b33
dario <dariokleinalves@gmail.com>
2026-09-11 13:13:30 -0300

    feat(structures): implementar busca e inclusão na árvore binária

--- structures/arvore_binaria.py
+++ structures/arvore_binaria.py
@@ -4,11 +4,38 @@ from .no import No
 class ArvoreBinaria:
     @staticmethod
     def buscar(raiz: No, codigo: int) -> No | None:
-        pass
+        atual: No | None = raiz
+        while atual is not None:
+            if codigo == atual.codigo:
+                return atual
+            if codigo < atual.codigo:
+                atual = atual.esquerda
+            else:
+                atual = atual.direita
+        return None
 
     @staticmethod
     def incluir(raiz: No, codigo: int, posicao: int) -> No:
-        pass
+        novo: No = No(codigo, posicao, None, None)
+        if raiz is None:
+            return novo
+        atual: No | None = raiz
+        pai: No | None = None
+
+        while atual is not None:
+            pai = atual
+            if codigo == atual.codigo:
+                raise ValueError("Já existe um registro com este código")
+            elif codigo < atual.codigo:
+                atual = atual.esquerda
+            else:
+                atual = atual.direita
+
+        if codigo < pai.codigo:
+            pai.esquerda = novo
+        else:
+            pai.direita = novo
+        return raiz
 
     @staticmethod
     def excluir(raiz: No, codigo: int) -> No | None:
