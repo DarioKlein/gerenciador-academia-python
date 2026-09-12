8a2121a26f13058887c10c286f369962ff0282c6
Dario Klein <dariokleinalves@gmail.com>
2026-09-12 01:41:47 -0300

    feat(structures): implementar exclusão na árvore binária

--- structures/arvore_binaria.py
+++ structures/arvore_binaria.py
@@ -38,5 +38,35 @@ class ArvoreBinaria:
         return raiz
 
     @staticmethod
+    def __menor(raiz: No) -> No | None:
+        atual: No | None = raiz
+
+        while atual.esquerda is not None:
+            atual = atual.esquerda
+
+        return atual
+
+    @staticmethod
     def excluir(raiz: No, codigo: int) -> No | None:
-        pass
+        if raiz is None:
+            return None
+
+        if codigo < raiz.codigo:
+            raiz.esquerda = ArvoreBinaria.excluir(raiz.esquerda, codigo)
+        elif codigo > raiz.codigo:
+            raiz.direita = ArvoreBinaria.excluir(raiz.direita, codigo)
+        else:
+            if raiz.esquerda is None and raiz.direita is None:
+                return None
+            elif raiz.esquerda is None:
+                aux: No = raiz.direita
+                return aux
+            elif raiz.direita is None:
+                aux: No = raiz.esquerda
+                return aux
+            else:
+                aux: No = ArvoreBinaria.__menor(raiz.direita)
+                raiz.codigo = aux.codigo
+                raiz.posicao = aux.posicao
+                raiz.direita = ArvoreBinaria.excluir(raiz.direita, aux.codigo)
+        return raiz
