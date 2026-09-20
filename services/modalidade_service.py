cf56c7558e9d62408daf6e866dbecacd39d902e0
Dario Klein <dariokleinalves@gmail.com>
2026-09-19 21:28:50 -0300

    feat(services): relacionar modalidades aos professores

--- services/modalidade_service.py
+++ services/modalidade_service.py
@@ -1,35 +1,55 @@
 from repositories import Repositorio, TipoRepositorio
-from models import Modalidade
+from models import Modalidade, Professor
 
 
-# Falta implementar as regras de negócio e validar o código de professor
 class ModalidadeService:
-    def __init__(self, repositorio_modalidades: Repositorio):
+    def __init__(
+        self, repositorio_modalidades: Repositorio, repositorio_professores: Repositorio
+    ):
         if not isinstance(repositorio_modalidades, Repositorio):
-            raise TypeError("O repositório informado é inválido")
+            raise TypeError("O repositório de modalidades informado é inválido")
+
+        if not isinstance(repositorio_professores, Repositorio):
+            raise TypeError("O repositório de professores informado é inválido")
 
         if repositorio_modalidades.tipo != TipoRepositorio.MODALIDADE:
             raise ValueError("O repositório informado não é de modalidades")
 
+        if repositorio_professores.tipo != TipoRepositorio.PROFESSOR:
+            raise ValueError("O repositório informado não é de professores")
+
         self.__repositorio_modalidades = repositorio_modalidades
+        self.__repositorio_professores = repositorio_professores
 
     def criar(self, modalidade: Modalidade) -> Modalidade:
         if not isinstance(modalidade, Modalidade):
             raise TypeError("A modalidade informada é inválida")
 
+        if self.__repositorio_professores.buscar(modalidade.cod_prof) is None:
+            raise ValueError("O professor informado não foi encontrado")
+
         self.__repositorio_modalidades.incluir(modalidade.para_dict())
         return modalidade
 
-    def buscar(self, codigo: int) -> Modalidade | None:
+    def buscar(self, codigo: int) -> tuple[Modalidade, Professor] | None:
         if not isinstance(codigo, int):
             raise TypeError("O código informado é inválido")
 
-        registro = self.__repositorio_modalidades.buscar(codigo)
+        registro_modalidade = self.__repositorio_modalidades.buscar(codigo)
 
-        if registro is None:
+        if registro_modalidade is None:
             return None
 
-        return Modalidade.dict_para_objeto(registro)
+        modalidade = Modalidade.dict_para_objeto(registro_modalidade)
+
+        registro_professor = self.__repositorio_professores.buscar(modalidade.cod_prof)
+
+        if registro_professor is None:
+            raise ValueError("O professor relacionado à modalidade não foi encontrado")
+
+        professor = Professor.dict_para_objeto(registro_professor)
+
+        return modalidade, professor
 
     def atualizar(self, codigo: int, modalidade_atualizada: Modalidade) -> Modalidade:
         if not isinstance(codigo, int):
@@ -37,6 +57,12 @@ class ModalidadeService:
 
         if not isinstance(modalidade_atualizada, Modalidade):
             raise TypeError("A modalidade informada é inválida")
+
+        if (
+            self.__repositorio_professores.buscar(modalidade_atualizada.cod_prof)
+            is None
+        ):
+            raise ValueError("O professor informado não foi encontrado")
 
         self.__repositorio_modalidades.atualizar(
             codigo, modalidade_atualizada.para_dict()
